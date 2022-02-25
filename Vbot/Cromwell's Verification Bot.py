dd40bf7f8e93bc3aaac3cf00b916da929b0326a0
Cromwell2025 <38790532+AReallyGenericUsername@users.noreply.github.com>
2022-02-25 13:07:21 -0700

    Version 1.4 Hotfix
    Screwed some stuff up. This just fixes that and adds the new $Version command

--- Vbot/Cromwell's Verification Bot.py
+++ Vbot/Cromwell's Verification Bot.py
@@ -12,20 +12,22 @@ intents = discord.Intents().all()
 workbook = load_workbook(filename="accountDB.xlsx")
 sheet = workbook.active
 client = discord.Client(intents=intents)
-url = 'emailwebhookhere'
-channel = client.get_channel(logchannelhere)
-cnc = client.get_channel(cncchannelhere)
+url = 'webhookhere'
+versionstr = '1.4'
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    channel = client.get_channel(logchannelhere)
     await channel.send('bot has come online')
 
     
 
 @client.event
 async def on_message(message):
-
+    channel = client.get_channel(logchannelhere)
+    cnc = client.get_channel(cncchannelhere)
+  
     def check(user):
         return user.author == message.author
     if message.author == client.user:
@@ -34,6 +36,7 @@ async def on_message(message):
     print('Message from {0.author}: {0.content}'.format(message))
     await channel.send('Message from {0.author.id} {0.author}: {0.content}'.format(message))
 
+      
     if message.content.startswith('$RTXERTWXYZ'):
         print('EMERGENCY SHUTOFF RECIEVED')
         print(message.channel)
@@ -42,6 +45,9 @@ async def on_message(message):
             response = await client.wait_for('message', check=check)
             if response.content == 'y':
                 sys.exit()
+        
+    if message.content.startswith('$Version'):
+        await message.channel.send(versionstr)
 
     if message.content.startswith('$Verify'):
         if isinstance(message.channel, discord.channel.DMChannel):
@@ -55,7 +61,7 @@ async def on_message(message):
         try:
                 
             await message.author.dm_channel.send(
-            f'Hi {message.author.name}, Please type in your [REDACTED] gmail. PLEASE NOTE: The bot only works with WCS-G accounts.'
+            f'Hi {message.author.name}, Please type in your [REDACTED] Gmail. PLEASE NOTE: The bot only works with WCS-G accounts.'
         )
 
             response = await client.wait_for('message', check=check)
@@ -123,12 +129,13 @@ async def on_message(message):
 @client.event 
 async def on_member_join(member):
     for value in sheet.iter_rows(values_only=True):
+        print(value)
         if str(member.id) in value:
             try:
                 var = discord.utils.get(member.guild.roles, name = "Verified")
                 await member.add_roles(var)
-            except:
-                print('error')
+            except Exception as e:
+                print('error' + e)
 
         
 client.run('TmljZSB0cnkgZnJlbmNoZnJ5ISBObyB0b2tlbiBoZXJlIQ==')
